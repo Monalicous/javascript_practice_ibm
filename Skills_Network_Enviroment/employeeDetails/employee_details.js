de4a618df0892aa63c40e4694e56db4baa7a2b38
Monalicous <samukelisiwentusi@outlook.com>
2024-05-06 22:53:48 +0400

    Updated with a new task

--- Skills_Network_Enviroment/employeeDetails/employee_details.js
+++ Skills_Network_Enviroment/employeeDetails/employee_details.js
@@ -1,12 +1,12 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JS'},
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Salaries' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Accounting'},
     //... More employee records can be added here
-    { id: 4, name: 'Candy Smith', age: 30, department: 'IT', salary: 50000 },
-    { id: 5, name: 'Ashley Jones', age: 27, department: 'Finance', salary: 60000 },
-    { id: 6, name: 'Bob Mark', age: 35, department: 'HR', salary: 45000 },
-    { id: 7, name: 'Candice Markhams', age: 30, department: 'IT', salary: 50000 },
+    { id: 4, name: 'Candy Smith', age: 30, department: 'IT', salary: 50000, specialization: 'Python' },
+    { id: 5, name: 'Ashley Jones', age: 27, department: 'Finance', salary: 60000, specialization: 'Auditing' },
+    { id: 6, name: 'Bob Mark', age: 35, department: 'HR', salary: 45000, specialization: 'EQ' },
+    { id: 7, name: 'Candice Markhams', age: 30, department: 'IT', salary: 50000, specialization: 'JAVA' },
   ];
 
 // Function to display all employees
@@ -27,20 +27,21 @@ function calculateTotalSalaries() {
 }
 
 //Displays it employees only
-function itEmployees(){
-  const ItEmployees = employees.filter(employee => employee.department === 'IT');
-  const itemployeesDisplay = ItEmployees.map((employee,index) => `<p>${employee.id}: ${employee.name}: ${employee.age} - ${employee.department} - $${employee.salary}</p>`).join('');
-document.getElementById('employeesDetails').innerHTML = itemployeesDisplay;
-
+function findEmployeeBySpecialization(employeespecialization){
+  const found = employees.find(employee => employee.specialization === employeespecialization);
+  if (found) {
+  document.getElementById('employeesDetails').innerHTML =`<p> ${found.name} ${found.department}: ${found.specialization} </p>`;
+  }else{
+    document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this speciality';
+  }
 }
 
 // find employee id button
 function findEmployeeById(employeeId){
     const foundEmployee = employees.find(employee => employee.id === employeeId);
     if (foundEmployee) {
-    document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+    document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.age} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
     }else{
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
     }
 }
-
